692794fe06d5d6fbb267b6bc2070235021d08c64
unknown <delkanoberutu8@gmail.com>
2025-03-05 22:47:03 +0700

    init

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(View view) {
+
+        }
+
+    }
+}
